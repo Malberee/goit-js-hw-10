fc54dfb7e6d87d7669ccf1380b2f82bb3be19d6e
Malberee <82259650+Malberee@users.noreply.github.com>
2023-03-22 15:42:37 +0200

    js-hw-10

---
+++ src/index.js
@@ -0,0 +1,82 @@
+import './css/styles.css';
+import { Notify } from 'notiflix';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
+
+const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  searchRef: document.querySelector('#search-box'),
+  countryListRef: document.querySelector('.country-list'),
+  countryInfoRef: document.querySelector('.country-info'),
+};
+
+refs.searchRef.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(e) {
+  const value = e.target.value.trim();
+
+  if (!value) {
+    clearMarkup();
+    return;
+  }
+
+  fetchCountries(value).then(data => {
+    if (data.length > 10 ) {
+      Notify.info('Too many matches found. Please enter a more specific name.');
+      return;
+    } else if (data.length > 1) {
+      renderCountryList(data);
+      return;
+    }
+
+    renderCountryInfo(data);
+    return;
+  }).catch(err => {
+    clearMarkup()
+    Notify.failure('Oops, there is no country with that name');
+  });
+}
+
+function renderCountryList(data) {
+  clearMarkup();
+
+  console.log(data);
+
+  data.forEach(country => {
+    refs.countryListRef.insertAdjacentHTML(
+      'beforeend',
+      `<li class="country-item">
+        <img src="${country.flags.svg}" class="country-flag"></img>
+        <h2>${country.name.official}</h2>
+      </li>`
+    );
+  });
+}
+
+function renderCountryInfo(data) {
+  clearMarkup();
+
+  console.log(data);
+
+  // const lang = data.languages.map(language => language.name).join(', ');
+
+  data.forEach(country => {
+    refs.countryInfoRef.insertAdjacentHTML(
+      'beforeend',
+      `<img src="${country.flags.svg}" class="country-flag"></img>
+      <h2>${country.name.official}</h2>
+      <p><span>Capital: </span>${country.capital}</p>
+      <p><span>Population: </span>${country.population}</p>
+      <p><span>Languages: </span>${Object.values(country.languages).join(
+        ', '
+      )}</p>
+      `
+    );
+  });
+}
+
+function clearMarkup() {
+  refs.countryInfoRef.innerHTML = '';
+  refs.countryListRef.innerHTML = '';
+}
